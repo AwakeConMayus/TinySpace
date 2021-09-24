c9055f165362142bc6f5ccc9bb8a5bbb509faccc
Clantanab <73425709+Clantanab@users.noreply.github.com>
2021-09-24 17:58:41 +0200

    creaacion de escena test

---
+++ Assets/Prefabs/test/CreateRoom.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoom : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Text _roomName;
+
+
+    public void OnClick_CreateRoom()
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom("basic", options, TypedLobby.Default);
+
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+    }
+}
