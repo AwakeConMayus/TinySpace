206da4b3ba743bcd3f6e03696cce3f57b5f26d86
DobleC <rexitah@gmail.com>
2021-09-28 16:15:05 +0200

    Añadidos más materiales + Cambiar el color del tablero
    WIP: De momento solo cambia los colores de las piezas al pulsarlas por testear.
    @RobertoGutierrezGuerrero he metido 3 colores de prueba en la misma carpeta donde está el color del tablero (el azul)

--- Assets/Scripts/Debug/ClickCasillas.cs
+++ Assets/Scripts/Debug/ClickCasillas.cs
@@ -8,10 +8,22 @@ public class ClickCasillas : MonoBehaviour
     Casilla CasillaHit;
     InstancePiezas instanciator;
 
+    MeshRenderer MeshCasilla;
+
+    [SerializeField]
+    Material ShaderAzul;
+    [SerializeField]
+    Material ShaderRojo;
+    [SerializeField]
+    Material ShaderVerde;
+    [SerializeField]
+    Material ShaderAmarillo;
+
     private void Start()
     {
         MainCamera = Camera.main;
         instanciator = GetComponent<InstancePiezas>();
+
     }
 
     void Update()
@@ -31,8 +43,21 @@ public class ClickCasillas : MonoBehaviour
                 CasillaHit = hit.collider.GetComponent<Casilla>();
                 instanciator.CrearPieza(CasillaHit);
 
+                MeshCasilla = hit.collider.GetComponentInChildren<MeshRenderer>();
+
+                switch (Random.Range(0, 3))
+                {
+                    case 0: MeshCasilla.material = ShaderRojo; break;
+                    case 1: MeshCasilla.material = ShaderVerde; break;
+                    case 2: MeshCasilla.material = ShaderAmarillo; break;
+                }
+                
+
             }
         } 
     }
+
+
+    
 
 }
