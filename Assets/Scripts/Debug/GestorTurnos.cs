96d4a0b07b623759a3aa2c766babfba56e83da30
DobleC <rexitah@gmail.com>
2021-10-01 16:38:04 +0200

    Añadidos comments

--- Assets/Scripts/Debug/GestorTurnos.cs
+++ Assets/Scripts/Debug/GestorTurnos.cs
@@ -6,9 +6,9 @@ public class GestorTurnos : MonoBehaviour
 {
     int turno = 1;
     int turnoAux = 1;
-    bool player = false;
 
-    //false = Player 1, true = Player 2
+    //* Jugador actual, false = Player 1, true = Player 2
+    bool player = false;
 
     private void Start()
     {
@@ -17,16 +17,16 @@ public class GestorTurnos : MonoBehaviour
 
     void realizarJugada()
     {
+        //* Si turnoAux es par, se invierte el jugador actual
         if (turnoAux % 2 == 0) player = !player;
 
         Debug.Log("Ha jugado Player = " + player);
         
         ++turnoAux;
 
-        //Debug.Log("TurnoAux = " + turnoAux);
-
         Debug.Log("Turno = " + turno);
 
+        //* Si turnoAux llega a 3, vuelve a ser 1 y se avanza al siguiente turno
         if (turnoAux == 3)
         {
             ++turno;
