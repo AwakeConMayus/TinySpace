408c50764224df25940b2dc071d71f4179d95b38
DobleC <rexitah@gmail.com>
2021-10-04 13:03:45 +0200

    Limitados los fps a 30
    En la camera hay un script llamado FPSTarget, ahí se puede editar la variable targetFPS para limitar los fps del juego.
    Esto es basicamente para reducir el consumo de recursos (y más teniendo en cuenta que el juego no es muy movido que digamos)

---
+++ Assets/Scripts/Auxiliares/FPSTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSTarget : MonoBehaviour
+{
+    //* Mantiene el juego a 30fps, principalmente para reducir el consumo de recursos
+    //* Teniendo en cuenta además que el juego no tiene mucho movimiento
+
+    [SerializeField]
+    private int targetFPS = 30;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFPS;
+    }
+
+    void Update()
+    {
+        if (Application.targetFrameRate != targetFPS)
+            Application.targetFrameRate = targetFPS;
+    }
+
+    void setFPS(int t) { targetFPS = t; }
+    int  getFPS() { return targetFPS; }
+
+}
