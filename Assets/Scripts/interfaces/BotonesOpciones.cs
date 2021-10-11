3dc950198bbeb3cfb799fbeb77509d3f5d95debc
Mikel Iturralde <57236319+mikelitu369@users.noreply.github.com>
2021-10-12 00:15:03 +0200

    no se

---
+++ Assets/Scripts/interfaces/BotonesOpciones.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BotonesOpciones : MonoBehaviour
+{
+    TextoOpciones textOpciones;
+    public int index;
+
+    
+    private void Start()
+    {
+        textOpciones = GetComponentInParent<TextoOpciones>();
+    }
+
+
+    private void OnMouseOver()
+    {
+        print("Entro en el boton");
+        switch (index)
+        {
+            case 1:
+                textOpciones.B1In();
+                break;
+            case 2:
+                textOpciones.B2In();
+                break;
+            case 3:
+                textOpciones.B3In();
+                break;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        print("Salgo del boton");
+        switch (index)
+        {
+            case 1:
+                textOpciones.B1Out();
+                break;
+            case 2:
+                textOpciones.B2Out();
+                break;
+            case 3:
+                textOpciones.B3Out();
+                break;
+        }
+    }
+}
