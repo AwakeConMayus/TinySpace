6bbb70ac94018f7136f852048dea7f6910c9735f
RobertoGutierrezGuerrero <robertoguti06@hotmail.com>
2021-10-13 02:41:14 +0200

    MeteoritosFixed+mejora escena gameplay

---
+++ Assets/Scripts/PequeñosScriptsDeRober/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Invoke("AutoDestruccion", 3f);
+    }
+   void AutoDestruccion()
+    {
+        Destroy(this.gameObject);
+    }
+}
