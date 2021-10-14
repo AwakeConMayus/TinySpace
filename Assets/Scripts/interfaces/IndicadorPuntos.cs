d1e0dcc482930b5a365b5953073c0082a586380a
Mikel Iturralde <57236319+mikelitu369@users.noreply.github.com>
2021-10-14 15:39:23 +0200

    interfaz puntos por nave

---
+++ Assets/Scripts/interfaces/IndicadorPuntos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IndicadorPuntos : MonoBehaviour
+{
+    public static IndicadorPuntos instance;
+
+    Text textoPuntos;
+    Canvas myCanvas;
+    public float posicionVertical;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+
+        textoPuntos = GetComponentInChildren<Text>();
+        myCanvas = GetComponentInParent<Canvas>();
+
+        EventManager.StartListening("ClickCasilla", Active);
+
+        gameObject.SetActive(false);
+    }
+
+
+    public void Active()
+    {
+        if (!ClickCasillas.casillaClick || !ClickCasillas.casillaClick.pieza) return;
+        CancelInvoke();
+        Pieza pieza = ClickCasillas.casillaClick.pieza;
+        gameObject.SetActive(true);
+        transform.localPosition = Camera.main.WorldToScreenPoint(pieza.transform.position) - myCanvas.transform.localPosition + new Vector3(0,posicionVertical,0);
+        textoPuntos.text = pieza.Puntos().ToString();
+        Invoke("Desactive", 2f);
+    }
+
+    public void Desactive()
+    {
+        gameObject.SetActive(false);
+    }
+}
