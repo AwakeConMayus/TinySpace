b887ddb62d0fe5fa59e09c1f3d14fdc6c886ed02
DobleC <rexitah@gmail.com>
2021-10-15 19:07:46 +0200

    Añadido menu facciones al titlescreen

--- Assets/Scripts/interfaces/TitleScreen.cs
+++ Assets/Scripts/interfaces/TitleScreen.cs
@@ -1,10 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    [SerializeField]
+    GameObject menuInicio;
+    [SerializeField]
+    GameObject menuFaccion;
+
+    Button[] btnFacciones;
+
+
+    int faccionSeleccionada;
+
+    private void Start()
+    {
+        menuInicio.SetActive(true);
+        menuFaccion.SetActive(false);
+        btnFacciones = new Button[5];
+
+        
+       btnFacciones = menuFaccion.GetComponentsInChildren<Button>();
+    }
+
+    public void ChangeButtons()
+    {
+        menuInicio.SetActive(false);
+        menuFaccion.SetActive(true);
+    }
+
     public void EnterMatchMaking()
     {
         SceneManager.LoadScene(1);
@@ -13,5 +40,18 @@ public class TitleScreen : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void selectFaccion(int faccion)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            btnFacciones[i].interactable = true;
+        }
+
+        faccionSeleccionada = faccion;
+        btnFacciones[faccionSeleccionada].interactable = false;
+
+        if (!btnFacciones[4].interactable) btnFacciones[4].interactable = true;
     }
 }
