bf848421bf1250b0e43ac3290704431a7bad8756
Clantanab <73425709+Clantanab@users.noreply.github.com>
2021-10-26 15:35:14 +0200

    scena para seleccionar faccion

---
+++ Assets/Scripts/interfaces/SeleccionDeFaccion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SeleccionDeFaccion : MonoBehaviour
+{
+
+
+    [SerializeField]
+    TuSeleccion mi_Seleccion;
+
+    private void Start()
+    {
+        mi_Seleccion.faccion = Faccion.none;
+        mi_Seleccion.mi_poder = null;
+
+        for(int i = 0; i < mi_Seleccion.mis_opciones.Length; ++i)
+        {
+            mi_Seleccion.mis_opciones[i] = null;
+        }
+    }
+
+
+    public void Seleccionar_Faccion(int i)
+    {
+        mi_Seleccion.faccion = (Faccion)i;
+        SceneManager.LoadScene(1);
+    }
+}
