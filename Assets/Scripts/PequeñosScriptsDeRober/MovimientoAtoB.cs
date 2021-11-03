dd24b44fb312913d263aa7596f52fc4e79ead42e
RobertoGutierrezGuerrero <robertoguti06@hotmail.com>
2021-11-03 01:00:37 +0100

    xd

---
+++ Assets/Scripts/PequeñosScriptsDeRober/MovimientoAtoB.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoAtoB : MonoBehaviour
+{
+    [SerializeField] Vector3 punto1;
+    [SerializeField] Vector3 punto2;
+    float speed = -0.1f;
+    Vector3 direccion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direccion =(punto1- punto2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimientoOnda();
+    }
+    void MovimientoOnda()
+    {
+        
+            this.transform.Translate(direccion * speed * Time.deltaTime); 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ( collision.collider.tag == "direccion")
+        {
+            print("me choque");
+            speed = speed * -1;
+        }
+    }
+}
