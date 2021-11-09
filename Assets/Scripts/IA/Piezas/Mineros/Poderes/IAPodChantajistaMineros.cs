13f0517e5aac83620b65377b4a75b36bf775b3ff
Mikel Iturralde <57236319+mikelitu369@users.noreply.github.com>
2021-11-09 16:11:08 +0100

    pequeñas updates

--- Assets/Scripts/IA/Piezas/Mineros/Poderes/IAPodChantajistaMineros.cs
+++ Assets/Scripts/IA/Piezas/Mineros/Poderes/IAPodChantajistaMineros.cs
@@ -12,6 +12,21 @@ public class IAPodChantajistaMineros : PoderIABase
         List<Casilla> casillasPosibles = FiltroCasillas.CasillasDeUnJugador(faccion, IATablero.instance.mapa);
         casillasPosibles = FiltroCasillas.CasillasAdyacentes(casillasPosibles, true);
         casillasPosibles = FiltroCasillas.CasillasDeOtroJugador(faccion, casillasPosibles);
+        casillasPosibles = FiltroCasillas.RestaLista(casillasPosibles, FiltroCasillas.CasillasDeUnTipo(Clase.astros, IATablero.instance.mapa));
+
+
+        foreach (Casilla c in casillasPosibles)
+        {
+            IATablero.instance.PrintInfoTablero(tabBase);
+            foreach (Casilla cc in FiltroCasillas.CasillasDeUnJugador(faccion, IATablero.instance.mapa))
+            {
+                IATablero.instance.PrintInfoTablero(tabBase);
+                Pieza aux = c.pieza;
+                c.pieza = cc.pieza;
+                cc.pieza = aux;
+                opciones.Add(new InfoTablero(IATablero.instance.mapa));
+            }
+        }
 
         return opciones;
     }
