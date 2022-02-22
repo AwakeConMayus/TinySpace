5a058329672b142ae76d2496b6f1bcb779d229b0
Miturralde <miturralde@simfor.es>
2022-02-22 18:26:06 +0100

    prefabs

---
+++ Assets/Scripts/IA/Piezas/Oyentes/Especiales/IATerraformador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IATerraformador : PiezaIA
+{
+    public int fase;
+
+    public override List<InfoTablero> Opcionificador(InfoTablero tabBase)
+    {
+        Pieza nuevoPlaneta = null;
+
+        switch (fase)
+        {
+            case 0:
+                nuevoPlaneta = Resources.Load<Pieza>("Planeta Sol Planetarios");
+                break;
+            case 1:
+                nuevoPlaneta = Resources.Load<Pieza>("Planeta Sol Planetarios");
+                break;
+            case 2:
+                nuevoPlaneta = Resources.Load<Pieza>("Planeta Sol Planetarios");
+                break;
+            case 3:
+                nuevoPlaneta = Resources.Load<Pieza>("Planeta Sol Planetarios");
+                break;
+
+        }
+
+        return null;
+    }
+}
