0fbf1311239a209d412559de1694b3489b4bdd4e
RobertoGutierrezGuerrero <73426108+RobertoGutierrezGuerrero@users.noreply.github.com>
2022-03-13 17:57:55 +0100

    Fills

---
+++ Assets/Scripts/PequeñosScriptsDeRober/MovimientoLineal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoLineal : MonoBehaviour
+{
+    [SerializeField] float PosicionInicial;
+    [SerializeField] float PosicionFinal;
+    [SerializeField] float FillRellenoInicial;
+    [SerializeField] float FillRellenoFinal;
+    [SerializeField] GameObject Fill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Fill.transform.position = new Vector3(Fill.transform.position.x, (PosicionFinal + (PosicionFinal - PosicionInicial) / (FillRellenoFinal - FillRellenoInicial) * (FillRellenoInicial - FillRellenoFinal)), Fill.transform.position.z);
+       
+    }
+}
+ 
