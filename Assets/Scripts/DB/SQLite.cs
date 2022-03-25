5e7320508de80a737404a8e478f1f374aa36655c
AwakeConMayus <87665439+AwakeConMayus@users.noreply.github.com>
2022-03-25 19:19:50 +0100

    sqlite

---
+++ Assets/Scripts/DB/SQLite.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class SQLite : MonoBehaviour
+{
+    private const string database_name = "my_db";
+
+    public string db_connection_string;
+    public IDbConnection db_connection;
+
+    public SQLite()
+    {
+        db_connection_string = "URI=file:" + Application.persistentDataPath + "/" + database_name;
+        Debug.Log("db_connection_string" + db_connection_string);
+        db_connection = new SqliteConnection(db_connection_string);
+        db_connection.Open();
+    }
+
+    ~SQLite()
+    {
+        db_connection.Close();
+    }
+
+    public IDbCommand getDbCommand()
+    {
+        return db_connection.CreateCommand();
+    }
+
+
+    public IDataReader getAllData(string table_name)
+    {
+        IDbCommand dbcmd = db_connection.CreateCommand();
+        dbcmd.CommandText =
+            "SELECT * FROM " + table_name;
+        IDataReader reader = dbcmd.ExecuteReader();
+        return reader;
+    }
+
+    public void close()
+    {
+        db_connection.Close();
+    }
+}
---
+++ Assets/Scripts/DB/VectoresDataBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+
+public class VectoresDataBase : SQLite
+{
+
+    private const string TABLE_NAME1 = "Totalpartidas";
+    private const string KEY_Partidas = "partidasJugadas";
+
+
+
+
+    private const string TABLE_NAME2 = "VectoresDB";
+    private const string KEY_VECTOR = "Vector";
+    private const string KEY_ACIERTO = "Acierto";
+    private const string KEY_FALLO = "Fallo";
+    private const string KEY_WINRATIO = "Winratio";
+
+
+    public VectoresDataBase() : base()
+    {
+        IDbCommand dbcmd = getDbCommand();
+        dbcmd.CommandText = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME1 + " ( " +
+            KEY_Partidas + " INT (10) )";
+        dbcmd.ExecuteNonQuery();
+
+
+        IDbCommand dbcmd2 = getDbCommand();
+        dbcmd2.CommandText = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME2 + " ( " +
+
+            KEY_VECTOR + " TEXT " +
+            KEY_ACIERTO + " INT (10) " +
+            KEY_FALLO + " INT (10) " +
+            KEY_WINRATIO + " FLOAT (10) )";
+        dbcmd2.ExecuteNonQuery();
+
+    }
+
+    public void addData(string vector)
+    {
+        /*IDbCommand dbcmd = getDbCommand();
+        dbcmd.CommandText =
+            "INSERT INTO " + TABLE_NAME2
+            + " ( "
+            + KEY_VECTOR + ", "
+            + KEY_ACIERTO + ", "
+            + KEY_FALLO + ", "
+            + KEY_WINRATIO + " ) "
+
+            + "VALUES ( '"
+            + vector+ "', " +
+            ", '"
+            + location._Lat + "', '"
+            + location._Lng + "' )";
+        dbcmd.ExecuteNonQuery();*/
+    }
+
+}
