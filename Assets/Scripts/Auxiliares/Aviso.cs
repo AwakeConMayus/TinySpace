36bd85ae48dc5c58a6c6d7be19e4f9c5ee7c4c99
Miturralde <57236319+mikelitu369@users.noreply.github.com>
2022-04-10 23:08:40 +0200

    a

---
+++ Assets/Scripts/Auxiliares/Aviso.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Aviso : MonoBehaviour
+{
+    float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 2 && Input.anyKeyDown || timer > 7)
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+}
